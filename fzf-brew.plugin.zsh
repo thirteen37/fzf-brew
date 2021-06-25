0570153ae26548a797653c4947333053beaa33d5
Yu-Xi Lim <limyuxi@gmail.com>
2021-06-25 18:36:16 +0800

    Provide trigger completions

--- fzf-brew.plugin.zsh
+++ fzf-brew.plugin.zsh
@@ -1,5 +1,40 @@
+#!/usr/bin/env zsh
+
+if ! (( $+commands[brew] )); then
+    echo 'brew command not found: please install via https://brew.sh/'
+    exit
+fi
+
+if ! (( $+commands[fzf] )); then
+    echo 'fzf command not found: please install via "brew install fzf"'
+    exit
+fi
+
+FB_FORMULA_PREVIEW='HOMEBREW_COLOR=true brew info {}'
+FB_FORMULA_BIND="ctrl-space:execute-silent(brew home {})"
+FB_CASK_PREVIEW='HOMEBREW_COLOR=true brew info --cask {}'
+FB_CASK_BIND="ctrl-space:execute-silent(brew home --cask {})"
+
+# completion bindings
+function _fzf_complete_brew() {
+    local arguments=$@
+
+    if [[ $arguments == 'brew install --cask'* ]]; then
+        _fzf_complete -m --preview $FB_FORMULA_PREVIEW --bind $FB_FORMULA_BIND -- "$@" < <(brew casks)
+    elif [[ $arguments == 'brew uninstall --cask'* ]]; then
+        _fzf_complete -m --preview $FB_FORMULA_PREVIEW --bind $FB_FORMULA_BIND -- "$@" < <(brew list --cask)
+    elif [[ $arguments == 'brew install'* ]]; then
+        _fzf_complete -m --preview $FB_FORMULA_PREVIEW --bind $FB_FORMULA_BIND -- "$@" < <(brew formulae)
+    elif [[ $arguments == 'brew uninstall'* ]]; then
+        _fzf_complete -m --preview $FB_FORMULA_PREVIEW --bind $FB_FORMULA_BIND -- "$@" < <(brew leaves)
+    else
+        eval "zle ${fzf_default_completion:-expand-or-complete}"
+    fi
+}
+
+# functions
 function fuzzy_brew_install() {
-    local inst=$(brew formulae | fzf --query="$1" -m --preview 'HOMEBREW_COLOR=true brew info {}' --bind="ctrl-space:execute-silent(brew home {})")
+    local inst=$(brew formulae | fzf --query="$1" -m --preview $FB_FORMULA_PREVIEW --bind $FB_FORMULA_BIND)
 
     if [[ $inst ]]; then
         for prog in $(echo $inst); do; brew install $prog; done;
@@ -7,7 +42,7 @@ function fuzzy_brew_install() {
 }
 
 function fuzzy_brew_uninstall() {
-    local uninst=$(brew leaves | fzf --query="$1" -m --preview 'HOMEBREW_COLOR=true brew info {}' --bind="ctrl-space:execute-silent(brew home {})")
+    local uninst=$(brew leaves | fzf --query="$1" -m --preview $FB_FORMULA_PREVIEW --bind $FB_FORMULA_BIND)
 
     if [[ $uninst ]]; then
         for prog in $(echo $uninst);
@@ -16,7 +51,7 @@ function fuzzy_brew_uninstall() {
 }
 
 function fuzzy_cask_install() {
-    local inst=$(brew casks | fzf --query="$1" -m --preview 'HOMEBREW_COLOR=true brew info --cask {}' --bind="ctrl-space:execute-silent(brew home --cask {})")
+    local inst=$(brew casks | fzf --query="$1" -m --preview $FB_CASK_PREVIEW --bind $FB_CASK_BIND)
 
     if [[ $inst ]]; then
         for prog in $(echo $inst); do; brew install --cask $prog; done;
@@ -24,7 +59,7 @@ function fuzzy_cask_install() {
 }
 
 function fuzzy_cask_uninstall() {
-    local inst=$(brew list --cask | fzf --query="$1" -m --preview 'HOMEBREW_COLOR=true brew info --cask {}' --bind="ctrl-space:execute-silent(brew home --cask {})")
+    local inst=$(brew list --cask | fzf --query="$1" -m --preview $FB_CASK_PREVIEW --bind $FB_CASK_BIND)
 
     if [[ $inst ]]; then
         for prog in $(echo $inst); do; brew uninstall --cask $prog; done;
@@ -38,12 +73,4 @@ function __setup_fzf_brew() {
     alias fcui=fuzzy_brew_uninstall
 }
 
-if (( $+commands[brew] )); then
-    if (( $+commands[fzf] )); then
-        __setup_fzf_brew
-    else
-        echo 'fzf command not found: please install via "brew install fzf"'
-    fi
-else
-    echo 'brew command not found: please install via https://brew.sh/'
-fi
+__setup_fzf_brew
